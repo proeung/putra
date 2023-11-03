e52fc0b244bd3400b01571deaf9b747e6155dea5
Putra Bonaccorsi <putra.roeung@gmail.com>
2023-11-03 12:02:02 -0400

    fix(google-analytics): adjust the loading of GA tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,25 +4,22 @@ import Script from "next/script"
 export default function Document() {
   return (
     <Html lang="en" className="scroll-smooth scroll-pt-[3.875rem] md:scroll-pt-20 dark">
-      <Script
-        strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-      />
-
-      <Script strategy="lazyOnload">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-          page_path: window.location.pathname,
-          });
-        `}
-      </Script>
       <Head />
       <body className="antialiased text-zinc-700 dark:text-zinc-300 bg-white dark:bg-slate-900">
         <Main />
         <NextScript />
+        <Script
+          strategy="lazyOnload"
+          src="https://www.googletagmanager.com/gtag/js?id=G-S4RYWZQ3MQ"
+        />
+        <Script strategy="lazyOnload">
+          {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-S4RYWZQ3MQ');
+            `}
+        </Script>
       </body>
     </Html>
   )
