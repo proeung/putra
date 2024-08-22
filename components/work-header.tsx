9a7b951b7a800114dbd476884370975184d1fcc6
Putra Bonaccorsi <putra.roeung@gmail.com>
2024-08-22 12:32:45 -0400

    fix(work-header): adjust browser frame on mobile & reduce margin

--- components/work-header.tsx
+++ components/work-header.tsx
@@ -28,7 +28,7 @@ const WorkHeader = ({ title, coverImage, coverVideo, poster, excerpt }: Props) =
           <div className="bg-slate-200 dark:bg-slate-950 -mx-5 overflow-hidden shadow-lg sm:mx-0 sm:rounded-lg">
             {
               coverVideo && (
-                <div className="w-full h-6 md:h-8 rounded-t-lg bg-gray-200 dark:bg-slate-800 flex justify-start items-center space-x-1.5 px-5 md:px-4">
+                <div className="w-full h-6 md:h-8 md:rounded-t-lg bg-gray-200 dark:bg-slate-800 flex justify-start items-center space-x-1.5 px-5 md:px-4">
                   <span className="w-1.5 h-1.5 rounded-full bg-red-400" />
                   <span className="w-1.5 h-1.5 rounded-full bg-yellow-400" />
                   <span className="w-1.5 h-1.5 rounded-full bg-green-400" />
@@ -50,7 +50,7 @@ const WorkHeader = ({ title, coverImage, coverVideo, poster, excerpt }: Props) =
             ) : null}
           </div>
 
-          <div className="max-w-3xl mx-auto mt-16 md:mt-40">
+          <div className="max-w-3xl mx-auto mt-10 md:mt-16 lg:mt-40">
             <p className="text-base/relaxed md:text-xl/relaxed">
               <span className="font-bold">{title}</span> - <span dangerouslySetInnerHTML={formattedExcerpt} />
             </p>
