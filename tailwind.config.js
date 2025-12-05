c88bb28d4ff8a5eb4d2b79318ed7be923810b678
Putra Bonaccorsi <putra.roeung@gmail.com>
2025-12-05 14:07:58 -0500

    chore(upgrade): update Next and migrate to Tailwind v4

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,53 +2,6 @@
 module.exports = {
   content: ['./components/**/*.tsx', './pages/**/*.tsx'],
   darkMode: 'class',
-  theme: {
-    container: {
-      center: true,
-      screens: {
-        sm: '100%',
-        md: '100%',
-        lg: '100%',
-        xl: '100%',
-        '2xl': '1680px'
-      }
-    },
-    extend: {
-      fontFamily: {
-        'sans': [
-          'SofiaPro',
-          'Segoe UI',
-          'Roboto',
-          'Helvetica Neue',
-          'sans-serif'
-        ],
-        'serif': [
-          'Playfair Display',
-          'Georgia',
-          'serif'
-        ]
-      },
-      fontSize: {
-        '5xl': '2.5rem',
-        '6xl': '2.75rem',
-        '7xl': '4.5rem',
-        '8xl': '6.25rem',
-      },
-      keyframes: {
-        animatedgradient: {
-          '0%': { backgroundPosition: '0% 50%' },
-          '50%': { backgroundPosition: '100% 50%' },
-          '100%': { backgroundPosition: '0% 50%' },
-        },
-      },
-      backgroundSize: {
-        '300%': '300%',
-      },
-      animation: {
-        gradient: 'animatedgradient 6s ease infinite alternate',
-      },
-    },
-  },
   plugins: [
     require('@tailwindcss/typography'),
   ],
