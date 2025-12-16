d1a9c9ffaf5c16d76cf242384db5dd3843930629
Putra Bonaccorsi <putra.roeung@gmail.com>
2025-12-16 14:25:46 -0500

    chore(package): add vercel/analytics

--- components/layout.tsx
+++ components/layout.tsx
@@ -2,6 +2,7 @@ import Header from './header';
 import Footer from './footer';
 import Meta from './meta';
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from '@vercel/analytics/react';
 
 type Props = {
   preview?: boolean
@@ -16,6 +17,7 @@ const Layout = ({ preview, children }: Props) => {
       <main className="min-h-screen overflow-hidden">
         {children}
         <SpeedInsights />
+        <Analytics />
       </main>
       <Footer />
     </>
